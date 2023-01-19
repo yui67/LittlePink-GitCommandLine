ea9578847e7e2f10066244fe4ff4b2b69e751e09
Mr.z <zhouc9025@gmail.com>
2023-01-20 01:25:59 +0800

    設置了背景

--- LittlePink-GitCommandLine/ViewController.swift
+++ LittlePink-GitCommandLine/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPurple
+        
+        
     }
+    
+    
 
 
 }
